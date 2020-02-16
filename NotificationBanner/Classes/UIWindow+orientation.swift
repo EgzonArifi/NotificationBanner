7df91e9b843bc09fb2cc25eed2fe215a6ee81d27
Egzon Arifi <arifieg@mobilenoc.mobi>
2020-02-16 16:06:28 +0100

    Update UIWindow+orientation.swift
    changed width and heigth calculation

--- NotificationBanner/Classes/UIWindow+orientation.swift
+++ NotificationBanner/Classes/UIWindow+orientation.swift
@@ -10,27 +10,11 @@ import UIKit
 extension UIWindow {
 
     public var width: CGFloat {
-        let orientation = UIDevice.current.orientation
-        switch orientation {
-        case .landscapeLeft, .landscapeRight:
-            return max(frame.width, frame.height)
-        case .portrait, .portraitUpsideDown:
-            return min(frame.width, frame.height)
-        default:
-            return frame.width
-        }
+        return frame.width
     }
-
+    
     public var height: CGFloat {
-        let orientation = UIDevice.current.orientation
-        switch orientation {
-        case .landscapeLeft, .landscapeRight:
-            return min(frame.width, frame.height)
-        case .portrait, .portraitUpsideDown:
-            return max(frame.width, frame.height)
-        default:
-            return frame.height
-        }
+        return frame.height
     }
 
 }
